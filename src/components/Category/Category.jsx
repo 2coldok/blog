109a9c9007d4b072aa6442ff70e6525ec8cad0f0
coldok <2coldok@gmail.com>
2024-03-01 18:32:20 +0900

    데이터 요청 실패로 인해 요청 방식 변경

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -12,18 +12,17 @@ export default function Category() {
   }
 
   useEffect(() => {
-    axios.get('/cat/dog.json')
-      .then((res) => setShow(res.data.price))
+    fetch('/cat/dog.json', {
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => setShow(data.price))
+
   }, [state])
   
-  // fetch('/cat/dog.json', {
-  //   headers: {
-  //     'Accept': 'application/json'
-  //   }
-  // })
-  // .then(res => res.json())
-  // .then(data => console.log(data))
- 
+
   return (
     <div className={styles.category}>
       <p>카테고리</p>
