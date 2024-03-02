5d06f5092f5e0819e5de85d7e3c816691eb47ad4
coldok <2coldok@gmail.com>
2024-03-03 01:31:33 +0900

    text데이터 요청 테스트

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,13 +2,13 @@ import styles from './Category.module.css'
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
-
+import ReactMarkdown from 'react-markdown';
 export default function Category() {
   const {isLoading, error, data} = useQuery({
     queryKey: ['dogs'],
     queryFn: async () => {
       return axios
-        .get('https://raw.githubusercontent.com/2coldok/blog-posts/main/css/css1.json')
+        .get('https://raw.githubusercontent.com/2coldok/blog-posts/main/css/css2.md')
         .then((res) => res.data)
     }
   })
@@ -18,6 +18,7 @@ export default function Category() {
       <p>{`로딩 상태: ${isLoading}`}</p>
       <p>{`에러 상태: ${error}`}</p>
       <p>{`데이터 : ${data}`}</p>
+      <ReactMarkdown>{data}</ReactMarkdown>
     </div>
   );
 }
