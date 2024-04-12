469cdadfa83d6d1e41e4f152bcec0ff1bcc2cf21
coldok <2coldok@gmail.com>
2024-04-12 17:42:55 +0900

    모바일 검색창 테스트

--- src/components/ToolkitModal.tsx
+++ src/components/ToolkitModal.tsx
@@ -63,6 +63,10 @@ const ModalContainer = styled.div<{ $active: boolean }>`
 
   @media (max-width: 768px) {
     width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    transform: none;
   }
 `;
 
