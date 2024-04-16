ccb4a3d82fa51a3f9658bb3b779e9cda1ecab339
coldok <2coldok@gmail.com>
2024-04-16 14:34:14 +0900

    feat : 재생중인 음악에 한하여 스타일링 적용기능 구현

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -14,23 +14,58 @@ export default function MenuList() {
   };
   
   return (
-    <>
-      <ListContainer>
-        {ArticlesData.map((articleData) => (
-          <ItemButton onClick={handleClick(articleData.category)}>{articleData.name}</ItemButton>
-        ))}
-      </ListContainer>
-    </>
+    <Wrapper>
+      <Header>
+        <ProfileImage src="https://2coldok.github.io/blog/image/profile.png" alt="profile" />
+        <ProfileImage src="https://2coldok.github.io/blog/image/github.png" alt="github" />
+        <ProfileImage src="https://2coldok.github.io/blog/image/gmail.png" alt="gmail" />
+
+        {/* <label>이찬웅</label> */}
+      </Header>
+      {ArticlesData.map((articleData) => (
+        <ItemButton onClick={handleClick(articleData.category)}>{articleData.name}</ItemButton>
+      ))}
+      ddcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdc
+    </Wrapper>
   )
 }
 
-const ListContainer = styled.div`
+const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 0.2em;
+`
+const Header = styled.div`
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  flex-direction: column;
+  margin: 1.5em 1em;
+
+  & > label {
+    margin-top: 0.6em;
+    color: #AEBACB;
+  }
+`
+
+const ProfileImage = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 0.6em;
+  background-color: white;
+  margin-bottom: 0.5em;
 `
 
 const ItemButton = styled.button`
-  margin: 0.5rem 0.5rem;
-  font-size: 2rem;
-  background-color: green;
+  margin: 0.2em;
+  font-size: 1em;
+  background-color: transparent;
+  color: #AEBACB;
+  padding: 0.4em 1em;
+  border-radius: 0.5em;
+  
+  
+  &:hover {
+    background-color: #1F262E;
+  }
 `
--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -4,26 +4,33 @@ import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
 import { musicOn, setUrl } from '../redux/slice/musicSlice';
 import { setMusicModal } from '../redux/slice/modalSlice';
-import { FaArrowRight } from "react-icons/fa";
-
+import { BsMusicPlayerFill } from "react-icons/bs";
+import { useState } from 'react';
 
 export default function Music() {
   const dispatch = useDispatch();
-  const handleMusicClick = (url: string) => () => {
+  const [currentTrack, setCurrentTrack] = useState('');
+  const handleMusicClick = (url: string, title: string) => () => {
     dispatch(setUrl(url));
     dispatch(musicOn());
     dispatch(setMusicModal(false));
+    setCurrentTrack(title);
   }
 
-  const handleCloseClick = () => {
-    dispatch(setMusicModal(false));
-  }
+  
     
   return (
     <MusicWrapper>
-      <CloseButton onClick={handleCloseClick}><FaArrowRight /></CloseButton>
+      
+      <Header>
+        <BsMusicPlayerFill />
+        <h1>Music List</h1>
+        
+        
+      </Header>
+      
       {MusicContents.map((music: IMusic) => (
-        <Button onClick={handleMusicClick(music.youtubeUrl)}>{music.title} - {music.singer}</Button>
+        <Button onClick={handleMusicClick(music.youtubeUrl, music.title)} $title={music.title} $track={currentTrack}>{music.title} - {music.singer}</Button>
       ))}
     </MusicWrapper>
   );
@@ -37,27 +44,54 @@ const MusicWrapper = styled.div`
   /* align-items: center;  */
 `
 
-const CloseButton = styled.button`
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
   margin-top: 20px;
   margin-bottom: 20px;
   height: 50px;
   border-style: none;
   background-color: #101418;
+  /* background-color: green; */
+  justify-content: center;
+  align-items: center;
+  color: #AEBACB;
+  border-radius: 1rem;
   & > svg {
-    color: #AEBACB;
-    font-size: 40px;
+    margin-right: 10px;
+    font-size: 35px;
   }
+
+  
 
   &:hover {
     filter: none;
   }
 `
 
-const Button = styled.button`
+// const CloseButton = styled.button`
+  
+//   margin-top: 20px;
+//   margin-left: 40px;
+//   height: 50px;
+//   border-style: none;
+//   background-color: #101418;
+//   & > svg {
+//     color: #AEBACB;
+//     font-size: 30px;
+    
+//   }
+
+//   &:hover {
+//     filter: none;
+//   }
+// `
+
+const Button = styled.button<{ $title: string, $track: string }>`
   margin: 0.2em;
   font-size: 1em;
   background-color: transparent;
-  color: #AEBACB;
+  color: ${props => props.$title === props.$track ? '#66B2FF' : '#AEBACB'};
   padding: 0.4em 1em;
   border-radius: 0.5em;
   
