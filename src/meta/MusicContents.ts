1734f96146d49c8eb71bd029d7509b751ca87fb4
coldok <2coldok@gmail.com>
2024-05-04 11:17:27 +0900

    chore : 음악 리스트 업데이트

--- src/meta/MusicContents.ts
+++ src/meta/MusicContents.ts
@@ -6,19 +6,47 @@ export interface IMusic {
 
 export const MusicContents: IMusic[] = [
   {
-    title: '처음 그 자리에',
-    singer: '풀하우스ost',
-    youtubeUrl: 'https://www.youtube.com/watch?v=2WyOq7IMEnc',
+    title: '소나기',
+    singer: '이클립스',
+    youtubeUrl: 'https://www.youtube.com/watch?v=0wHduLk0Obw',
+
+  },
+  {
+    title: '그래, 늘 그랬듯 언제나',
+    singer: '이창섭',
+    youtubeUrl: 'https://www.youtube.com/watch?v=D7JMPrn10lc',
+
+  },
+  {
+    title: '청춘찬가',
+    singer: '세븐틴',
+    youtubeUrl: 'https://www.youtube.com/watch?v=e4tlvgKtcuw'
+
+  },
+  {
+    title: '꿈결같아서 1시간',
+    singer: '민니',
+    youtubeUrl: 'https://www.youtube.com/watch?v=iI2umq2iWuI&t=1168s'
+  },
+  {
+    title: 'Why',
+    singer: '태연',
+    youtubeUrl: 'https://www.youtube.com/watch?v=WkdtmT8A2iY',
   },
   {
     title: '일기 1시간',
-    singer: '김냐영',
+    singer: '김나영',
     youtubeUrl: 'https://www.youtube.com/watch?v=ZE0OH7tz9SU&t=281s',
   },
   {
-    title: '나에게로의 초대 1시간',
-    singer: '조유진 X 박가영',
-    youtubeUrl: 'https://www.youtube.com/watch?v=wCiysUZn0us',
+    title: '인사',
+    singer: '범진',
+    youtubeUrl: 'https://www.youtube.com/watch?v=bbqr6NkJ88A',
+  },
+  {
+    title: '천상연',
+    singer: '이창섭',
+    youtubeUrl: 'https://www.youtube.com/watch?v=OwPoj6KUdxo',
   },
   {
     title: '어느 날 우리',
@@ -26,8 +54,53 @@ export const MusicContents: IMusic[] = [
     youtubeUrl: 'https://www.youtube.com/watch?v=wyVDJwh8-YU',
   },
   {
-    title: '천상연',
-    singer: '이창섭',
-    youtubeUrl: 'https://www.youtube.com/watch?v=OwPoj6KUdxo',
+    title: '삐삐',
+    singer: '아이유',
+    youtubeUrl: 'https://www.youtube.com/watch?v=Vb1htKLQ-UE',
+
   },
+  {
+    title: '후라이의 꿈',
+    singer: '악동뮤지션',
+    youtubeUrl: 'https://www.youtube.com/watch?v=3kGAlp_PNUg',
+
+  },
+  {
+    title: 'Walk Away',
+    singer: 'Mina Okabe',
+    youtubeUrl: 'https://www.youtube.com/watch?v=YewXMEsaTEw',
+
+  },
+  {
+    title: '사건의 지평선 1시간',
+    singer: '윤하',
+    youtubeUrl: 'https://www.youtube.com/watch?v=sCuCg_-PyWc'
+  },
+  {
+    title: 'Steal The Show',
+    singer: '엘리멘탈 OST',
+    youtubeUrl: 'https://www.youtube.com/watch?v=AJsvGtGgI6M',
+  },
+  {
+    title: 'To.X',
+    singer: '태연',
+    youtubeUrl: 'https://www.youtube.com/watch?v=Myz6pdW07O0',
+
+  },
+  {
+    title: '첫눈',
+    singer: '엑소',
+    youtubeUrl: 'https://www.youtube.com/watch?v=mHe3amVvtVo',
+
+  },
+  {
+    title: '우리의 꿈',
+    singer: '코요태',
+    youtubeUrl: 'https://www.youtube.com/watch?v=fT4RdQMrWvQ',
+  },
+  {
+    title: '플레이리스트 테스트',
+    singer: '',
+    youtubeUrl: 'https://www.youtube.com/watch?v=fO36kiw-bUE&list=RDMM&start_radio=1&rv=AJsvGtGgI6M',
+  }
 ];
